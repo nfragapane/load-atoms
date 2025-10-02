7f157ca04a5dfbf4e3815c4cb5bd2efa1e861978
Natascia Fragapane <quee4073@epsilon.chem.ox.ac.uk>
2025-10-02 15:02:59 +0100

    add importers

---
+++ src/load_atoms/database/importers/lips_25.py
@@ -0,0 +1,11 @@
+from load_atoms.database.backend import BASE_GITHUB_URL, SingleFileImporter
+from load_atoms.database.internet import FileDownload
+
+
+class Importer(SingleFileImporter):
+    @classmethod
+    def file_to_download(cls) -> FileDownload:
+        return FileDownload(
+            url=f"{BASE_GITHUB_URL}/LiPS-25/lips-25.extxyz",
+            expected_hash="c299a6bfd3ac38510488700a29fd8c910598a536fe77f4b9de4e6b08bf6ba62c",
+        )
